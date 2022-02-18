e9779446eecdaeef3a93359886d3c60214100b83
mirotadzelilu <davidqutateladze@gmail.com>
2022-02-18 18:19:33 +0400

    full

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+// davaleba 1 
+// let masivi = [5, 25, 89, 120, 36];
+// masivi.push('javascript', 'python');
+// masivi.unshift('html', 'css');
+// console.log(masivi.length);
+// masivi.shift();
+// masivi.pop();
+// console.log(masivi);
+
+// davaleba 2
+// let xili = ['orange', 'banana', 'pear'];
+// console.log(xili.length);
+// console.log(xili);
+// xili.push('apple', 'pineapple');
+// xili.unshift('watermalon');
+// console.log(xili.length)
+// console.log(xili);
+// xili.splice(2,0, 'mango');
+// let axaliXili = xili.slice(1,6);
+// console.log(axaliXili.length);
+// console.log(axaliXili);
+
+// davaleba3
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newArray = array.map(division => division / 3);
+// console.log(newArray);
+
+// davaleba 4 
+// let sitkvebi = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+// let sitkvebi2 = sitkvebi.filter(function(item){
+//     return typeof item === 'string';
+// });
+// console.log(sitkvebi2);
+
+// davaleba 5
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+// let result = languages.filter(item => item.length > 3);
+// console.log(result);
+
+// davaleba 6
+// let akademia = ['academy', 'of', 'digital', 'industries'].reduce(function(accumulator, value){
+//     return accumulator.concat(value)
+// },
+// '' 
+// );
+// console.log(akademia);
+
+// davaleba 7
+
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+
+// davaleba 8
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let qalaqebi = words.filter(item => item.includes('M'));
+// console.log(qalaqebi);
